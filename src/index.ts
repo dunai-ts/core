6621884fa270896fe9ee83d7a284b1740dfbe240
Лубяной Евгений <arswarog@yandex.ru>
2019-06-04 10:22:16 +0300

    Fix

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@
  * The library may work in browser and on Node.js
  */
 export * from './lib/DependencyTree';
+export * from './lib/ParamDecoration';
 export * from './lib/Injector';
 export * from './lib/Service';
 export * from './lib/Common';
